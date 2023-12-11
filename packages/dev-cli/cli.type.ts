0297542f8d2ef7df547d1902f1c563fdafc9316d
Alexis-ROBERT <alexisrobert.github@gmail.com>
2023-12-11 07:00:42 +0100

    Created of interface

---
+++ packages/dev-cli/cli.type.ts
@@ -0,0 +1,17 @@
+export interface IOptionContruct {
+        name: string;
+
+        description?: string;
+}
+
+export interface IResultOK {
+        reason?: string;
+
+        isOk: boolean;
+}
+
+export interface ICLIInfos {
+        name: string;
+
+        description?: string;
+}
