f79845c717af6cece06e8e5cbef19c7067f9c3b8
Alexis-ROBERT <alexisrobert.github@gmail.com>
2023-12-12 07:34:16 +0100

    initialized method and added of new methods.

--- packages/dev-cli/cli.ts
+++ packages/dev-cli/cli.ts
@@ -4,13 +4,13 @@ import { IOptionContruct, IResultOK } from './cli.type';
  *
  */
 export default class CommandLineInterface {
-        private _optionContruct: IOptionContruct[];
-        private _optionExec: string[];
+        private _optionContruct: IOptionContruct[] = [];
+        private _optionExec: string[] = [];
 
-        private _ok: boolean;
-        private _reason: string;
+        private _ok: boolean = false;
+        private _reason: string = '';
 
-        private _stepContruct: any | any[];
+        private _stepContruct: any[] = [];
 
         public constructor(private _name: string) {
                 this._ok ?? false;
@@ -19,6 +19,8 @@ export default class CommandLineInterface {
         public name(): string {
                 return this._name;
         }
+
+        
 
         public ok(isOk?: boolean, reason?: string): IResultOK | boolean {
                 if (isOk === undefined) {
@@ -46,26 +48,39 @@ export default class CommandLineInterface {
                 };
         }
 
-        public step(callBack: () => any): this {
+        public step(callBack: (cli: this) => any): this {
+                this._stepContruct.push(callBack);
                 return this;
         }
 
-        public call(): any | any[] {}
+        public call(): any | any[] {
+                this._stepContruct.forEach((step) => {
+                        step(this);
+                })
+        }
 
         public pushOptionForExecute(name: string): this {
                 this._optionExec.push(name);
                 return this;
         }
 
+        public cLIValidate(cli: CommandLineInterface): boolean {
+            if (!RegExp(/^[a-zA-Z0-9_-]+$/).test(cli.name())) {
+                    return false;
+            }
+
+            return true;
+    }
+
         public exec(callBack?: (cli: this) => any): void {
                 if (this._ok) {
+                        callBack(this);
+
                         this._optionExec.forEach((e) =>
                                 this._optionContruct.forEach((o) => {
                                         if (e === o.name) o.action(this);
                                 })
                         );
-
-                        callBack(this);
                 }
         }
 
@@ -80,7 +95,8 @@ export default class CommandLineInterface {
 
         public option(name?: string, description?: string, action?: (cli: this) => any): IOptionContruct[] {
                 if (name !== undefined) {
-                        if (RegExp().test()) {
+                        if (RegExp(/^[a-zA-Z0-9_]+$/).test(name)) {
+                                
                                 this._optionContruct.push({
                                         name: name,
                                         description: description,
