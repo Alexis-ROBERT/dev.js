adc013c80434fc7068b7eb202c167b240b34b62a
Alexis-ROBERT <128067166+Alexis-ROBERT@users.noreply.github.com>
2023-12-14 07:26:55 +0100

    Created command for create component

---
+++ packages/dev-cli/create-component/angular/sources/cli.ts
@@ -0,0 +1,7 @@
+import CommandLineInterfaceConstruct from "../../../sources/cli.constructor";
+
+export default class AngularCreateComponent extends CommandLineInterfaceConstruct {
+        constructor() {
+                super('create-component')
+        }
+}
---
+++ packages/dev-cli/create-component/react/sources/cli.ts
@@ -0,0 +1,7 @@
+import CommandLineInterfaceConstructor from '../../../sources/cli.constructor';
+
+export default class ReactCreateComponent extends CommandLineInterfaceConstructor {
+        public constructor() {
+                super('create-component');
+        }
+}
